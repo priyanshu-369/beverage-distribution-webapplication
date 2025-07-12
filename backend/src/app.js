ce270d08d4e605348ad6f7fb8aaacab82c8e9819
priyanshu-369 <priyanshugupta15993097@gmail.com>
2025-07-12 09:47:42 +0530

    some middleware configured in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,26 @@
 import express from "express";
-
+import cookieParser from "cookie-parser"
+import errorHandler from "./middlewares/errorHandler.middleware.js"
 const app = express();
 
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true  
+}));
+
+// niche hum configure kiya hai ki express sirf json ka data limit 16kb hai 
+app.use(express.json({limit:"16kb"}));
+
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+// ye hum koi file ko locally apne server pe store karte waqt use karenge 
+app.use(express.static("public"))
+app.use(cookieParser())
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
+app.use(errorHandler)
 export default app
