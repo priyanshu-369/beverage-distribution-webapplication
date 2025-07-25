be791f6b403d7c4fea66bf0c2283775b2aa07d1f
priyanshu-369 <priyanshugupta15993097@gmail.com>
2025-07-25 17:00:48 +0530

    feat(product): add routes for updating product details and images; implement delete functionality for products

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,10 +1,19 @@
 import { Router } from "express"
-import { createNewProduct, getAllProduct, getProductById } from "../controllers/product.controller.js"
+import { createNewProduct, deleteProductById, getAllProduct, getProductById, updateProductDetail, updateProductImage } from "../controllers/product.controller.js"
 import upload from "../middlewares/multer.middleware.js"
 const router = Router()
 
 //product create karne images ke sath
-router.route("/create").post(upload.fields({name:productImage, maxCount: 2}), createNewProduct)
+router.route("/create").post(upload.fields({name: "productImage", maxCount: 2}), createNewProduct)
+
+//ye route update karega sirf product details ko
+router.route("/update/details/:id").put(updateProductDetail)
+
+//ye route specifically sirf product image update karega
+router.route("/update/image/:id").patch(upload.fields({name: "productImage", maxCount: 2}), updateProductImage)
+
+//ye route poduct id se usko delete karega
+router.route("/delete/:id").delete(deleteProductById)
 
 //it will handle query and simple get All product request
 router.route("/").get(getAllProduct)
